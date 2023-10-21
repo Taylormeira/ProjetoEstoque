831ec128996b8548a4a71091e426110bed04814e
Taylormeira <taylortmeira25@gmail.com>
2023-10-21 00:48:47 -0300

    Criado mock na tabela de estoque para testes, adicionado Page Ccontroller para funções globais e ajustado integração da tela principal para tela de estoque.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/telas/principalPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/telas/PrincipalPage.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
